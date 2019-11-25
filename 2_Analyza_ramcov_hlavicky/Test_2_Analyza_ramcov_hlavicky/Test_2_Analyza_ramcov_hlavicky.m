f1ba377d2e752a2cf89e86e7236812c501427415
Dobroslav Tokoš <dtokos@zoznam.sk>
2019-11-25 09:48:00 +0100

    Created project forr second assignment

---
+++ 2_Analyza_ramcov_hlavicky/Test_2_Analyza_ramcov_hlavicky/Test_2_Analyza_ramcov_hlavicky.m
@@ -0,0 +1,37 @@
+//
+//  Test_2_Analyza_ramcov_hlavicky.m
+//  Test_2_Analyza_ramcov_hlavicky
+//
+//  Created by dtokos on 25/11/2019.
+//  Copyright © 2019 dtokos. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test_2_Analyza_ramcov_hlavicky : XCTestCase
+
+@end
+
+@implementation Test_2_Analyza_ramcov_hlavicky
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
